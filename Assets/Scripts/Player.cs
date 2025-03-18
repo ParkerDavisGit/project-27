97bdaf12eac0b8dc09a1241079ff0ed5df1e4283
parker <parker.e.davis@gmail.com>
2025-03-18 10:51:53 -0400

    upload initially

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Variables
+    private int speed;
+    private PlayerAction action;
+    private Vector2 moveDirection;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
